cbac84d869f9f58cc5d5d5b06b8f6b8cb661d27f
sidemt <25644062+sidemt@users.noreply.github.com>
2018-09-04 19:57:20 +0000

    Modify page title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = 'On The Day'
+  end
+
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', @base_title.to_s
   end
 end
