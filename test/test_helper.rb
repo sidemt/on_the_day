e22630683f52b358897dfa04d6926d97cb1541b9
sidemt <25644062+sidemt@users.noreply.github.com>
2018-09-04 20:25:25 +0000

    Install guard

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 module ActiveSupport
   class TestCase
