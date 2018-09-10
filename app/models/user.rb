8dad230d67796dde4aadd023bee01db68e57458c
sidemt <25644062+sidemt@users.noreply.github.com>
2018-09-10 18:17:24 +0000

    Add day column

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
-  
+
   def self.find_or_create_from_oauth(auth)
     User.find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.user_name   = auth.info.nickname
+      user.user_name = auth.info.nickname
       user.avatar_url = auth.info.image
       user.email      = User.dummy_email(auth)
       user.password   = Devise.friendly_token[0, 20]
