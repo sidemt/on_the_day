3db52dc10b731d6b6f853205456c907705eec0eb
sidemt <25644062+sidemt@users.noreply.github.com>
2018-09-12 17:40:30 +0000

    Requre login to access my page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user, only: [:my_page]
+
   def home
     @day = params[:day]
     respond_to do |format|
@@ -9,5 +11,14 @@ class StaticPagesController < ApplicationController
   def my_page
     @user = current_user
     @day = current_user.day
+  end
+
+  private
+
+  # before action
+  def logged_in_user
+    return if user_signed_in?
+    flash[:danger] = 'Please log in.'
+    redirect_to root_url
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
   def setup
+    Warden.test_mode!
+    @user = users(:one)
     @base_title = 'On The Day'
   end
 
@@ -11,7 +14,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'title', @base_title.to_s
   end
 
-  test 'should get my_page' do
+  test 'should redirect my_page when not logged in' do
+    get my_page_path
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should get my_page when logged in' do
+    login_as(@user, scope: :user)
     get my_page_path
     assert_response :success
   end
