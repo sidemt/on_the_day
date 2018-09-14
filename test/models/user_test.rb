38a55286bd34f3023697cb05852e10c2b27d3b92
sidemt <25644062+sidemt@users.noreply.github.com>
2018-09-14 19:35:17 +0000

    Add tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Warden::Test::Helpers
+  def setup
+    Warden.test_mode!
+    @user = users(:one)
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'avatar_url should be converted to https' do
+    @user.save
+    assert_equal('https://example.com/image.jpg', @user.avatar_url)
+  end
 end
