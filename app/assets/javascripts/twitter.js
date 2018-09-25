d9a798dcfd4bd55465665bfe22aeefa44f7119de
sidemt <25644062+sidemt@users.noreply.github.com>
2018-09-25 18:08:55 +0000

    Change the timing of loading widget.js

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -1,20 +1,12 @@
-$(document).ready(function() {
+function twitterWidget( jQuery ) {
   // load the widgets.js file
-  window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-      t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
-  
-    return t;
+  window.twttr = (function (d, s, id) {
+    var t, js, fjs = $("body");
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src= "https://platform.twitter.com/widgets.js";
+    $(fjs).append(js, fjs);
+    return window.twttr || (t = { _e: [], ready: function (f) { t._e.push(f)  }  });
   }(document, "script", "twitter-wjs"));
   
   // Define our custom event handlers
@@ -23,8 +15,10 @@ $(document).ready(function() {
   }
   
   // Wait for the asynchronous resources to load
-  twttr.ready(function (twttr) {
+  window.twttr.ready(function (twttr) {
     // Now bind our custom intent events
     twttr.events.bind('tweet', increaseDay);
   });
-});
+}
+
+$(document).on('turbolinks:load', twitterWidget);
